36411fab239bb03fe3a42f2be3856dfeff196fb2
PedroBRosa <pedro.brosa01@gmail.com>
2023-03-15 21:13:30 -0300

    Incremente and return JSON into ejs

---
+++ pratica1/src/controllers/login.js
@@ -0,0 +1,8 @@
+
+module.exports.index = function(application, req, res) {
+    var loginModel = new application.src.models.login();
+  
+    loginModel.getLogin(function(err, result) {
+      res.render("login/index", {login : result});
+    });
+  }
